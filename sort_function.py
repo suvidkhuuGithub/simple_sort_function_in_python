3d22928c249fb99a868a1a55079e85c6f281a5a5
suvidkhuuGithub <98112683+suvidkhuuGithub@users.noreply.github.com>
2025-10-24 16:17:13 -0700

    Delete sort_function.py

--- sort_function.py
+++
@@ -1,6 +0,0 @@
-def sort(w, h, l, m):
-    if (max(w, h, l) >= 150 or w * h * l >= 1_000_000) and m >= 20:
-        return "REJECTED"
-    if max(w, h, l) >= 150 or w * h * l >= 1_000_000 or m >= 20:
-        return "SPECIAL"
-    return "STANDARD"
